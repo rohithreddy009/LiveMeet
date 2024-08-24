b5f9f95f5cec028600a64d987d6a3a8015cf08fc
Rohith Reddy <89250524+rohithreddy009@users.noreply.github.com>
2024-08-24 14:51:11 +0530

    clerk ui change

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,13 +29,13 @@ export default function RootLayout({
             socialButtonsVariant: "iconButton",
             logoImageUrl: "/icons/yoom-logo.svg",
           },
-          variables: {
-            colorText: "#fff",
-            colorPrimary: "#0E78F9",
-            colorBackground: "#1C1F2E",
-            colorInputBackground: "#252A41",
-            colorInputText: "#fff",
-          },
+          // variables: {
+          //   colorText: "#fff",
+          //   colorPrimary: "#0E78F9",
+          //   colorBackground: "#fca503",
+          //   colorInputBackground: "#252A41",
+          //   colorInputText: "#fff",
+          // },
         }}
       >
         <body className={`${inter.className} bg-dark-2`}>
